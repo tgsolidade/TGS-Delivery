a1791c307aa65457e5742a836efb50abb9aa182f
Tales Solidade <talessolidade@yahoo.com>
2026-04-06 20:12:49 -0300

    Versão definitiva sem firebase.exe

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,40 @@
-importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js",
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js",
+);
 
 // Suas credenciais atuais do Firebase
 firebase.initializeApp({
-    apiKey: "AIzaSyDLSvxNn8jwZGG8Ky9MUlDRvSH6vHK7vdI",
-    authDomain: "base-delivery-1d420.firebaseapp.com",
-    projectId: "base-delivery-1d420",
-    storageBucket: "base-delivery-1d420.firebasestorage.app",
-    messagingSenderId: "734860193156",
-    appId: "1:734860193156:web:b013890e8351b10b72c1d1"
+  apiKey: "AIzaSyDLSvxNn8jwZGG8Ky9MUlDRvSH6vHK7vdI",
+  authDomain: "base-delivery-1d420.firebaseapp.com",
+  projectId: "base-delivery-1d420",
+  storageBucket: "base-delivery-1d420.firebasestorage.app",
+  messagingSenderId: "734860193156",
+  appId: "1:734860193156:web:b013890e8351b10b72c1d1",
 });
 
 const messaging = firebase.messaging();
 
 // ESSE É O CARA QUE ESCUTA A NOTIFICAÇÃO QUANDO O APP ESTÁ FECHADO/MINIMIZADO
 messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Notificação recebida em segundo plano!', payload);
-    
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/icon-192.png',
-        badge: '/icon-192.png',
-        vibrate: [600, 200, 600, 200, 1500],
-        requireInteraction: true // Faz a notificação ficar na tela até o motoboy clicar nela
-    };
+  console.log(
+    "[firebase-messaging-sw.js] Notificação recebida em segundo plano!",
+    payload,
+  );
 
-    return self.registration.showNotification(notificationTitle, notificationOptions);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/icon-192.png",
+    badge: "/icon-192.png",
+    vibrate: [600, 200, 600, 200, 1500],
+    requireInteraction: true, // Faz a notificação ficar na tela até o motoboy clicar nela
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions,
+  );
 });
